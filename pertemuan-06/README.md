701fcc61dcc39f5858e22ec66eab373a463fa743
unknown <2511500025@mahasiswa.atmaluhur.ac.id>
2025-11-03 15:41:44 +0700

    styling selector ID #contact input,#contact textarea

--- pertemuan-06/README.md
+++ pertemuan-06/README.md
@@ -3,6 +3,9 @@ NIM : 2511500025 <br>
 Nama : Teuku Maulana Yunus <br> 
 Hari ini, Rabu 30 oktober 2025, 
 saya mempelajari:<br>
+<ol>
 <li>hello.php</li>
+<li>copy index html, style css, script.js (pertemuan-05) </li>
+</ol>
 
 
---
+++ pertemuan-06/index.php
@@ -0,0 +1,81 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Judul Halaman</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Ini Header</h1>
+        <button class="menu-toggle" id="menuToggle" aria-label="Toggle Navigation">
+            &#9776;
+        </button>
+        <nav>
+            <ul>
+                <li><a href="#home">Beranda</a></li>
+                <li><a href="#about">Tentang</a></li>
+                <li><a href="#contact">Kontak</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section id="home">
+            <h2>Selamat Datang</h2>
+            <p>Ini contoh paragraf HTML</p>
+            <P>Halo! Selamat datang di halaman saya!</P>
+            <?php
+echo "halo dunia";
+?>
+        </section>
+
+        <section id="about">
+            <h2>Tentang Kami</h2>
+            <p><strong>NIM:</strong> 2511500025</p>
+            <p><strong>Nama Lengkap:</strong> Teuku Maulana Yunus &#128526;</p>
+            <p><strong>Tempat Lahir: </strong>Cilegon </p>
+            <p><strong>Tanggal Lahir:</strong> 27,Agustus 2005</p>
+            <p><strong>Hobby:</strong>Berbisnis & Berolahraga &#127926;</p>
+            <p><strong>Pasangan:</strong> Alvina Syafitri &hearts;</p>
+            <p><strong>Pekerjaan:</strong>Mahasiswa & Berbisnis</p>
+            <p><strong>Nama Orang tua</strong> Elly Novida & Muhammad Yunus</p>
+            <p><strong>Nama kakak:</strong>N Cut Yayang Tari</p>
+        </section>
+
+        <section id="contact">
+            <h2>Kontak Kami</h2>
+
+            <form action="" method="get" novalidate>
+                <label> <span>Nama:</span>
+                    <input type="text" id="txtNama" name="txtNama" placeholder="Masukkan nama" required
+                        autocomplete="name">
+                </label>
+
+                <label>
+                    <span>Email:</span>
+                    <input type="email" id="txtEmail" name="txtEmail" placeholder="Masukkan email" required
+                        autocomplete="email">
+                </label>
+
+                <label>
+                    <span>Pesan:</span>
+                    <textarea name="txtPesan" id="txtPesan" rows="4" placeholder="Tulis pesan anda..."
+                        required></textarea>
+                </label>
+
+                <button type="submit">Kirim</button>
+                <button type="reset">Batal</button>
+            </form>
+        </section>
+    </main>
+
+    <footer>
+        <p>&copy; 2025 TEUKU MAULANA YUNUS [2511500025]</p>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
