cb5c9cedea9ed283685337c3bbbf40d754741c1a
unknown <2511500025@mahasiswa.atmaluhur.ac.id>
2025-11-05 11:20:05 +0700

    styling selector ID #contact input,#contact textarea

--- pertemuan-06/index.php
+++ pertemuan-06/index.php
@@ -23,25 +23,71 @@
     </header>
     <main>
         <section id="home">
-            <h2>Selamat Datang</h2>
-            <p>Ini contoh paragraf HTML</p>
-            <P>Halo! Selamat datang di halaman saya!</P>
+        
+            
             <?php
-echo "halo dunia";
-?>
+            echo "halo dunia";
+            echo "halo perkenalkan nama saya teuku maulana yunus";
+            ?>
+            <P>Ini contoh paragraf HTML.</P>
         </section>
 
         <section id="about">
-            <h2>Tentang Kami</h2>
-            <p><strong>NIM:</strong> 2511500025</p>
-            <p><strong>Nama Lengkap:</strong> Teuku Maulana Yunus &#128526;</p>
-            <p><strong>Tempat Lahir: </strong>Cilegon </p>
-            <p><strong>Tanggal Lahir:</strong> 27,Agustus 2005</p>
-            <p><strong>Hobby:</strong>Berbisnis & Berolahraga &#127926;</p>
-            <p><strong>Pasangan:</strong> Alvina Syafitri &hearts;</p>
-            <p><strong>Pekerjaan:</strong>Mahasiswa & Berbisnis</p>
-            <p><strong>Nama Orang tua</strong> Elly Novida & Muhammad Yunus</p>
-            <p><strong>Nama kakak:</strong>N Cut Yayang Tari</p>
+            <?php
+            $nim = "2511500025";
+            $nama = "Teuku Maulana Yunus &#128526;";
+            $tempatlahir = "Cilegon";
+            $tanggallahir = "27 Agustus 2005";
+            $hobby = "Berbisnis & Berolahraga &#127926;";
+            $pasangan = "Alvina Syafitri &hearts;";
+            $pekerjaan = "Mahasiswa & Berbisnis";
+            $namaorangtua = "Elly Novida & Muhammad Yunus";
+            $namakakak= "Cut yayang Tari";
+            $namaadik = "-";
+            ?>
+            <h2>Tentang Saya</h2>
+            <p><strong>NIM:</strong> 
+        <?php
+        echo $nim;
+        ?>
+        </p>
+            <p><strong>Nama Lengkap:</strong><?php
+        echo $nama;
+        ?>
+        </p>
+            <p><strong>Tempat Lahir:</strong><?php
+        echo $tempatlahir;
+        ?>
+         </p>
+            <p><strong>Tanggal Lahir:</strong><?php
+        echo $tanggallahir;
+        ?>
+        </p>
+            <p><strong>Hobby:</strong> <?php
+        echo $hobby;
+        ?>
+         </p>
+            <p><strong>Pasangan:</strong><?php
+        echo $pasangan;
+        ?>
+        </p>
+            <p><strong>Pekerjaan:</strong><?php
+        echo $pekerjaan;
+        ?>
+        </p>
+            <p><strong>Nama Orang tua</strong> <?php
+        echo $namaorangtua;
+        ?>
+        </p>
+            <p><strong>Nama kakak:</strong> <?php
+        echo $namakakak;
+        ?>
+        </p>
+            <p><strong>Nama adik:</strong>
+        <?php
+        echo $namaadik;
+        ?>
+        </p>
         </section>
 
         <section id="contact">
@@ -68,6 +114,46 @@ echo "halo dunia";
                 <button type="submit">Kirim</button>
                 <button type="reset">Batal</button>
             </form>
+        </section>
+        <section id="ipk">
+            <h2>Nilai Saya</h2>
+<?php
+            $namaMatkul1 = "Kalkulus";
+        $namaMatkul2 = "Logika Informatika";
+        $namaMatkul3 = "Pengantar Teknik Informatika";
+        $namaMatkul4 = "Aplikasi Perkantoran";
+        $namaMatkul5 = "Konsep Basis Data";
+
+        $sksMatkul1 = "3";
+        $sksMatkul2 = "3";
+        $sksMatkul3 = "3";
+        $sksMatkul4 = "3";
+        $sksMatkul5 = "3";
+
+        $nilaiHadir1 = "90";
+        $nilaiHadir2 = "100";
+        $nilaiHadir3 = "100";
+        $nilaiHadir4 = "80";
+        $nilaiHadir5 = "100";
+
+        $nilaiTugas1 = "90";
+        $nilaiTugas2 = "100";
+        $nilaiTugas3 = "100";
+        $nilaiTugas4 = "90";
+        $nilaiTugas5 = "80";
+
+        $nilaiUTS1 = "70";
+        $nilaiUTS2 = "80";
+        $nilaiUTS3 = "80";
+        $nilaiUTS4 = "90";
+        $nilaiUTS5 = "80";
+
+        $nilaiUAS1 = "80";
+        $nilaiUAS2 = "90";
+        $nilaiUAS3 = "90";
+        $nilaiUAS4 = "100";
+        $nilaiUAS5 = "90";
+?>
         </section>
     </main>
 
